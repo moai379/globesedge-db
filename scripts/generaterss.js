1751340204259fe23fbe61c2894a8dd9c4647a32
Rajat Tyagi <trajat24156@gmail.com>
2026-02-10 22:18:37 +0530

    xml complaincy

--- scripts/generaterss.js
+++ scripts/generaterss.js
@@ -24,7 +24,7 @@ const items = postsData.posts
   .join("\n");
 
 const rss = `<?xml version="1.0" encoding="UTF-8"?>
-<rss version="2.0">
+<rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom">
   <channel>
     <atom:link href="https://raw.githubusercontent.com/moai379/globesedge-db/main/rss.xml" rel="self" type="application/rss+xml"/>
     <title>${FEED_TITLE}</title>
